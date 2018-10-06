64ad6116605f357211a7f604affe96741b65bb02
InkoHX <inkohx@gmail.com>
2018-10-06 21:49:25 +0900

    スマホだとバグり始めたのでなくす

--- js/core.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    particleground(document.getElementById('particles'), {
-        dotColor: '#ffffff',
-        lineColor: '#8dB0ed'
-    });
-    var intro = document.getElementById('intro');
-    intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
