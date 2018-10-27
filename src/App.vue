6144b63b6d6f91db79a39075136f00b9d5ca6c86
InkoHX <inkohx@gmail.com>
2018-10-28 03:20:26 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,57 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app class="blue-grey">
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="orange--text">Vector</span>
+        <span class="blue--text">Network</span>
+        <span class="font-weight-light yellow--text">Project</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">GitHub</span>
+      </v-btn>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">PLAYING</span>
+      </v-btn>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Discord</span>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <section>
+        <Home></Home>
+      </section>
+    </v-content>
+    <MyFooter></MyFooter>
+  </v-app>
 </template>
 
 <script>
+import MyFooter from './components/MyFooter'
+import Home from './pages/home'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MyFooter,
+    Home
+  },
+  data () {
+    return {
+      //
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
