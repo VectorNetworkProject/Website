ce94b9a351a8f60395754da4ada6e40bc5315b16
InkoHX <inkohx@gmail.com>
2018-11-11 06:38:20 +0900

    fix font

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,33 @@
 <template>
-  <v-parallax
-    dark
-    src="https://www.vector-network.tk/assets/home/Marble.jpg"
-  >
+  <div id="home">
+    <v-parallax
+      dark
+      src="https://www.vector-network.tk/assets/home/Marble.jpg"
+    >
+      <v-layout
+        align-center
+        justify-center
+        column
+      >
+        <h1 class="display-2 font-weight-regular mb-3 black--text">VectorNetwork</h1>
+        <h4 class="subheading black--text">MinecraftBE server development project</h4>
+      </v-layout>
+    </v-parallax>
     <v-layout
       align-center
-      column
       justify-center
     >
-      <h1 class="display-2 font-weight-thin mb-3 black--text">VectorNetwork</h1>
-      <h4 class="subheading black--text">MinecraftBE server development project</h4>
+      
     </v-layout>
-  </v-parallax>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      
+    }
   }
 }
 </script>
