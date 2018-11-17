61dffe71618c86e0781e3db3e528f322b8178209
InkoHX <inkohx@gmail.com>
2018-11-17 20:15:39 +0900

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <v-layout
+      align-center
+      column
+    >
+      <v-flex
+        md6
+        xs12
+        sm8
+        text-xs-center
+      >
+        <v-flex
+          v-if="error.statusCode === 404"
+          class="display-4"
+        >4🤔4</v-flex>
+        <v-flex
+          v-else
+          class="display-4"
+        >{{ error.statusCode }}</v-flex>
+        <br>
+        <v-btn
+          color="red"
+          href="/"
+        >ホームに戻る</v-btn>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
