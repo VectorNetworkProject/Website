5081280e158cf91dd4f131ab5676ea7f10575e85
InkoHX <inkohx@gmail.com>
2018-12-01 13:33:50 +0900

    文字の色を変更

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,8 @@
         justify-center
         column
       >
-        <h1 class="display-2 font-weight-regular mb-3 black--text">VectorNetwork</h1>
-        <h4 class="subheading black--text">MinecraftBE server development project</h4>
+        <h1 class="display-2 font-weight-regular mb-3 white--text">VectorNetwork</h1>
+        <h4 class="subheading  white--text">MinecraftBE server development project</h4>
         <v-btn
           href="minecraft://?addExternalServer=§l§6Vector §bNetwork|play.vector-network.tk:19132"
           class="green lighten-2 mt-5">Play</v-btn>
