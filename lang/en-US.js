3a2f1260231e8b476b554b50d4a71e95b536448e
InkoHX <inkohx@gmail.com>
2018-12-23 17:43:03 +0900

    Fix i18n

---
+++ lang/en-US.js
@@ -0,0 +1,11 @@
+export default {
+  index: {
+    title: 'The Mix',
+    subtext: 'ベクトルサーバーの新しいゲーム！',
+    subtitle: 'here we go! Let\'s play with VectorNetwork!',
+    buttons: {
+      stable: '遊ぶ(安定)',
+      latest: '遊ぶ(最新)'
+    }
+  }
+}
---
+++ lang/ja-JP.js
@@ -0,0 +1,11 @@
+export default {
+  index: {
+    title: 'The Mix',
+    subtext: 'ベクトルサーバーの新しいゲーム！',
+    subtitle: 'さあ！あなたもVectorNetworkで遊ぼう！',
+    buttons: {
+      stable: '遊ぶ(安定)',
+      latest: '遊ぶ(最新)'
+    }
+  }
+}
