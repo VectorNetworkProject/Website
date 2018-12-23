eca90e57d39835376fa7f88551d78b29c5134573
InkoHX <inkohx@gmail.com>
2018-12-23 18:13:35 +0900

    Update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,18 +17,21 @@ module.exports = {
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'description', name: 'description', content: 'MinecraftBEのゲームサーバーVectorNetwork公式サイトです。'},
       {name: 'author', content: 'VectorNetworkProject'},
+      {name: 'keywords', content: 'VectorNetwork'},
       {name: 'twitter:card', content: 'summary_large_image'},
       {name: 'twitter:url', content: 'https://www.vector-network.tk'},
+      {name: 'twitter:image', content: './home/Network.png'},
+      {name: 'twitter:site', content: '@Vector_Network_'},
+      {name: 'twitter:creator', content: '@InkoHX'},
       {name: 'msapplication-TileColor', content: '#bcbcbc'},
       {name: 'theme-color', content: '#bcbcbc'},
-      {
-        property: 'og:image',
-        content: 'home/Network.png'
-      }
+      {property: 'og:image', content: './home/Network.png'}
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'},
+      {rel: 'shortcut icon', href: './favicon.ico'},
+      {rel: 'canonical', href: 'https://www.vector-network.tk'}
     ]
   },
 
