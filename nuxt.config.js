7e6502ab7d18730ba414f5ebf5101fb874fde597
InkoHX <inkohx@gmail.com>
2018-12-23 17:33:03 +0900

    i18n(国際化)

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,26 +13,29 @@ module.exports = {
   head: {
     title: 'Vector Network Project',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'MinecraftBEのゲームサーバーVectorNetwork公式サイトです。' },
-      { name: 'author', content: 'VectorNetworkProject' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:url', content: 'https://www.vector-network.tk' },
-      { name: 'msapplication-TileColor', content: '#bcbcbc' },
-      { name: 'theme-color', content: '#bcbcbc' },
-      { property: 'og:image', content: 'https://github.com/VectorNetworkProject/Website/blob/master/assets/home/Vector_Network.png?raw=true' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'MinecraftBEのゲームサーバーVectorNetwork公式サイトです。'},
+      {name: 'author', content: 'VectorNetworkProject'},
+      {name: 'twitter:card', content: 'summary_large_image'},
+      {name: 'twitter:url', content: 'https://www.vector-network.tk'},
+      {name: 'msapplication-TileColor', content: '#bcbcbc'},
+      {name: 'theme-color', content: '#bcbcbc'},
+      {
+        property: 'og:image',
+        content: 'https://raw.githubusercontent.com/VectorNetworkProject/Website/master/static/home/Network.png'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
     ]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
 
   /*
   ** Global CSS
@@ -45,7 +48,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/vuetify'
+    '~/plugins/vuetify'
   ],
 
   /*
@@ -54,6 +57,18 @@ module.exports = {
   modules: [
     ['@nuxtjs/google-analytics', {
       id: 'UA-113467747-2'
+    }],
+    ['nuxt-i18n', {
+      locales: [
+        {code: 'ja', iso: 'ja_JP', file: 'ja-JP.js'},
+        {code: 'en', iso: 'en-US', file: 'en-US.js'},
+      ],
+      defaultLocale: 'ja',
+      vueI18n: {
+        fallbackLocale: 'ja',
+      },
+      lazy: true,
+      langDir: 'lang/'
     }],
     '@nuxtjs/axios'
   ],
--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,21 @@
       dark
       src="home/Network.png"
     >
-      <v-layout
-        align-center
-        justify-center
-        column
-      >
-        <h1 class="display-2 font-weight-regular mb-3 white--text">The Mix</h1>
-        <h4 class="subheading  white--text">Original game</h4>
-        <v-btn
-          href="minecraft://?addExternalServer=§l§6Vector §bNetwork|play.vector-network.tk:56333"
-          class="grey darken-1 mt-5">Play</v-btn>
+      <v-layout class="align-center">
+        <v-flex>
+          <h1 class="display-1">{{ $t('index.title') }}</h1>
+          <span class="subheading">{{ $t('index.subtext') }}</span>
+          <v-divider class="my-3"/>
+          <div class="title mb-3">{{ $t('index.subtitle') }}</div>
+          <v-btn
+            href="minecraft://?addExternalServer=§l§6Vector §bNetwork|play.vector-network.tk:56333"
+            class="grey darken-1 mt-5"
+          >{{ $t('index.buttons.stable') }}</v-btn>
+          <v-btn
+            href="minecraft://?addExternalServer=§l§6Vector §bNetwork|play.vector-network.tk:56333"
+            class="grey darken-1 mt-5"
+          >{{ $t('index.buttons.latest') }}</v-btn>
+        </v-flex>
       </v-layout>
     </v-parallax>
   </div>
