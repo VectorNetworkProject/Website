2c8a1b773ce3c8698cc588c263ea086c131c03ca
InkoHX <inkohx@gmail.com>
2018-12-23 18:49:41 +0900

    Fix

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,11 @@
           <v-divider class="my-3"/>
           <div class="title mb-3">{{ $t('index.subtitle') }}</div>
           <v-btn
-            href="minecraft://?addExternalServer=§l§6Vector §bNetwork§a(Stable)|play.vector-network.tk:56333"
+            href="minecraft://?addExternalServer=VectorNetwork(S)|play.vector-network.tk:56333"
             class="green darken-1 mt-5"
           >{{ $t('index.buttons.stable') }}</v-btn>
           <v-btn
-            href="minecraft://?addExternalServer=§l§6Vector §bNetwork§c(Latest)|c.mirm.jp:32605"
+            href="minecraft://?addExternalServer=VectorNetwork(L)|c.mirm.jp:32605"
             class="red darken-1 mt-5"
           >{{ $t('index.buttons.latest') }}</v-btn>
         </v-flex>
